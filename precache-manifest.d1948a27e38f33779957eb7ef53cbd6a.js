f9d80d7d2f596923d1472af5f26ef7366590ccb0
Camellia Hua <camelliahua@gmail.com>
2019-06-25 15:59:20 -0400

    Updates

---
+++ precache-manifest.d1948a27e38f33779957eb7ef53cbd6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818f2f003880f93c5c456294be68a760",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6b3185111f164452eed",
+    "url": "/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "b3639117edc190f07961",
+    "url": "/static/css/main.87f18995.chunk.css"
+  },
+  {
+    "revision": "c6b3185111f164452eed",
+    "url": "/static/js/2.59ce4812.chunk.js"
+  },
+  {
+    "revision": "b3639117edc190f07961",
+    "url": "/static/js/main.145522d8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
